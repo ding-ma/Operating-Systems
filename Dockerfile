e908772260857019fc393101d31ae6e609c04ac0
Ding Ma <ding.ma@mail.mcgill.ca>
2020-09-19 12:40:09 -0400

    tutorial 1 works

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,3 @@
-# CLion remote docker environment (How to build docker container, run and stop it)
-#
-# Build and run:
-#   docker build -t clion/remote-cpp-env:0.5 -f Dockerfile .
-#   docker run -d --cap-add sys_ptrace -p127.0.0.1:3333:22 --name clion_remote_env clion/remote-cpp-env:0.5
-#   ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:3333"
-#
-# stop:
-#   docker stop clion_remote_env
-#
-# ssh credentials (test user):
-#   user@password
-
 FROM ubuntu:18.04
 
 RUN apt-get update \
--- Tutorial-1/Client.c
+++ Tutorial-1/Client.c
@@ -8,42 +8,51 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-const char end = 26;
+const char kEndOfTransfer = 26;
 
-int main(int argc, char *argv[]){
-    if (argc != 3){
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         return EXIT_FAILURE;
     }
     int serverPort = atoi(argv[2]), fdSocketClient;
-    if ((fdSocketClient = socket(PF_INET, SOCK_STREAM,0))<0){
-        perror("client socket created error \n");
+    // init the socket
+    if ((fdSocketClient = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("client socket create error\n");
         return EXIT_FAILURE;
     }
+    // init connection info
     struct sockaddr_in clientAddress;
-    memset(&clientAddress, 0, sizeof( struct sockaddr_in));
+    memset(&clientAddress, 0, sizeof(struct sockaddr_in));
     clientAddress.sin_family = AF_INET;
+    // set server's IP address
     inet_pton(AF_INET, argv[1], &clientAddress.sin_addr);
-    clientAddress.sin_port = htons(serverPort); //packing server ip
-    if (connect(fdSocketClient, (struct sockaddr *)(&clientAddress), sizeof(struct sockaddr))){
-        perror("client socket connect error \n");
+    // set server's port
+    clientAddress.sin_port = htons(serverPort);
+    // connect to server using information above
+    if (connect(fdSocketClient, (struct sockaddr *) (&clientAddress), sizeof(struct sockaddr))) {
+        perror("client socket connect error\n");
         return EXIT_FAILURE;
     }
-    
     char *inputLine = NULL;
     ssize_t inputLineLengthRead = -1;
     size_t inputLengthAllocated = 0;
+    // prompt user's input command until EOF
     while ((inputLineLengthRead = getline(&inputLine, &inputLengthAllocated, stdin)) != -1) {
-        send(fdSocketClient, inputLine, inputLineLengthRead,0);
-        char receiveChar;
-        while(recv(fdSocketClient, &receiveChar,1,0)>0){
-            if(receiveChar == end) break;
-            putc(receiveChar, stdout);
+        // send command to server
+        send(fdSocketClient, inputLine, inputLineLengthRead, 0);
+        // print/forward everything sent from server to console
+        char recvChar;
+        while (recv(fdSocketClient, &recvChar, 1, 0) > 0) {
+            if (recvChar == kEndOfTransfer) break;
+            putc(recvChar, stdout);
         }
         fflush(stdout);
+        // free up & reinitialize inputLine and inputLengthAllocated used by getline
         free(inputLine);
         inputLine = NULL;
         inputLengthAllocated = 0;
     }
+    // close socket to avoid file descriptor exaustion problem (email Prof. Muthucumaru Maheswaran for more detail regarding this problem)
     close(fdSocketClient);
     return EXIT_SUCCESS;
 }
