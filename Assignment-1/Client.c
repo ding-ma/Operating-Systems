fe665105458741668fab1cf87ab4d9658521e464
Ding Ma <ding.ma@mail.mcgill.ca>
2020-09-23 00:43:04 -0400

    client done

--- Assignment-1/Client.c
+++ Assignment-1/Client.c
@@ -1,109 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 #include "a1_lib.h"
 
+#define BUFSIZE   1024
 
-const char kEndOfTransfer = 26;
-
-
-void sendCommand(int fdSocketClient,char* inputLine, char inputLineLengthRead) {
-    send(fdSocketClient, inputLine, inputLineLengthRead, 0);
-    
-    // print/forward everything sent from server to console
-    char recvChar;
-    while (recv(fdSocketClient, &recvChar, 1, 0) > 0) {
-        if (recvChar == kEndOfTransfer){
-            break;
-        }
-        putc(recvChar, stdout);
-    }
-}
-
-//void sendCommandIfValid(char *cmd, int numbArgs, int fdSocketClient, char* inputLine, char inputLineLengthRead) {
-//    int counter = 0;
-//    cmd = strtok(NULL, " ");
-//    while (cmd != NULL) {
-//        printf("TOKEN------- %s\n", cmd); //printing each token
-//        cmd = strtok(NULL, " ");
-//        counter++;
-//    }
-//    if(counter == numbArgs){
-//        sendCommand(fdSocketClient,inputLine, inputLineLengthRead);
-//    } else{
-//        printf("There is an error in your command \n");
-//    }
-//}
-
-
-int isCommandValid(char* cmd){
-    char *commands[] = {"add", "multiply", "divide", "sleep","factorial"};
-    int len = sizeof(commands) / sizeof(commands[0]);
-    for (int i = 0; i < len; ++i) {
-        if (strcmp(commands[i], cmd) == 0) {
-            return 1;
-        }
-    }
-    return 0;
-}
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         return EXIT_FAILURE;
     }
-    
-    int serverPort = atoi(argv[2]), fdSocketClient;
-    // init the socket
-    if ((fdSocketClient = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    int sockfd;
+    char user_input[BUFSIZE] = { 0 };
+    char server_msg[BUFSIZE] = { 0 };
+    if (connect_to_server(argv[1], atoi(argv[2]), &sockfd) < 0) {
         perror("client socket create error\n");
         return EXIT_FAILURE;
     }
-    // init connection info
-    struct sockaddr_in clientAddress;
-    memset(&clientAddress, 0, sizeof(struct sockaddr_in));
-    clientAddress.sin_family = AF_INET;
-    // set server's IP address
-    inet_pton(AF_INET, argv[1], &clientAddress.sin_addr);
-    // set server's port
-    clientAddress.sin_port = htons(serverPort);
-    // connect to server using information above
-    if (connect(fdSocketClient, (struct sockaddr *) (&clientAddress), sizeof(struct sockaddr))) {
-        perror("client socket connect error\n");
-        return EXIT_FAILURE;
-    }
-    char *inputLine = NULL;
-    ssize_t inputLineLengthRead = 0;
-    size_t inputLengthAllocated = 0;
-    // prompt user's input command until EOF
-    while (inputLineLengthRead != -1) {
-        // send command to server
+    
+    while (strcmp(user_input, "exit\n")) {
+        memset(user_input, 0, sizeof(user_input));
+        memset(server_msg, 0, sizeof(server_msg));
+        
+        // read user input from command line
         printf(">> ");
-        inputLineLengthRead = getline(&inputLine, &inputLengthAllocated, stdin);
+        fgets(user_input, BUFSIZE, stdin);
         
-        char duplicate[300];
-        strcpy(duplicate,inputLine);
-        char *token = strtok(inputLine, " ");
-        
-        if (isCommandValid(token)) { //check if the command is a valid one
-            sendCommand(fdSocketClient, duplicate, inputLineLengthRead);
-        } else if (strcmp(token, "exit\n") == 0) {
+        //receive a msg from the server. we will send everything to the server and it will process it
+        send_message(sockfd, user_input, strlen(user_input));
+        ssize_t byteCount = recv_message(sockfd, server_msg, sizeof(server_msg));
+        if (byteCount <= 0) {
             break;
-        } else {
-            printf("Wrong command \n");
         }
-        
-        fflush(stdout);
-        // free up & reinitialize inputLine and inputLengthAllocated used by getline
-        free(inputLine);
-        inputLine = NULL;
-        inputLengthAllocated = 0;
+        printf("Server: %s\n", server_msg);
     }
-    // close socket to avoid file descriptor exaustion problem (email Prof. Muthucumaru Maheswaran for more detail regarding this problem)
-    close(fdSocketClient);
+    
     return EXIT_SUCCESS;
 }
