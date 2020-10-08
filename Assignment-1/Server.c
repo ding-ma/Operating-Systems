856ff202dfa2e7716aa40e3238f195ac2b8706d8
ding-ma <ding.ma@mail.mcgill.ca>
2020-10-07 21:53:26 -0400

    shared memeory works but cant close server

--- Assignment-1/Server.c
+++ Assignment-1/Server.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/mman.h>
 #include "a1_lib.h"
 
 #define BUFSIZE   2024
-
+#define NUMBER_CLIENT 10
 
 int addInts(char *cmd) {
     char *token = strtok(cmd, " ");
@@ -28,7 +28,7 @@ int multiplyInts(char *cmd) {
 }
 
 int isValidDivition(char *cmd) {
-    fprintf(stderr, "in valid div %s\n", cmd);
+//    fprintf(stderr, "in valid div %s\n", cmd);
     char *token = strtok(cmd, " ");
     float x = atof(token);
     token = strtok(NULL, " ");
@@ -77,115 +77,168 @@ int isCommandValid(char *cmd) {
 }
 
 int isInputLengthValid(char *cmd, int expectedLength) {
-    fprintf(stderr, "command in %s", cmd);
     char *token = strtok(cmd, " ");
     int curr = 0;
     while (token != NULL) {
         token = strtok(NULL, " ");
         curr++;
     }
-    fprintf(stderr, "numb command %d", curr);
     return curr == expectedLength;
 }
+
+int isEqual(int *arr) {
+    for (int i = 1; i < NUMBER_CLIENT; ++i) {
+        if (arr[i] != 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         return EXIT_FAILURE;
     }
     
+    int *processes = mmap(NULL, NUMBER_CLIENT * sizeof(int),
+                          PROT_READ | PROT_WRITE,
+                          MAP_SHARED | MAP_ANONYMOUS,
+                          0, 0);
+    
+    if (processes == MAP_FAILED) {
+        printf("Mapping Failed\n");
+        return 1;
+    }
+    // for the signals 1 means busy, 0 means idle
+    processes[0] = 1; //we dont shutdown
+    int processCounter = 1; //first one is reserved for shutdown signal
+    
     int sockFd, clientFd;
     char msg[BUFSIZE];
     char msgCopy[BUFSIZE];
     char answer[BUFSIZE];
+    int server = create_server(argv[1], atoi(argv[2]), &sockFd);
     message *sentMsg;
-    if (create_server(argv[1], atoi(argv[2]), &sockFd) < 0) {
+    if (server < 0) {
         perror("listen socket create error\n");
         return EXIT_FAILURE;
     }
 //    freopen("/dev/null", "w", stderr); //surpress debug mode
     fprintf(stderr, "Server listening on %s:%s \n", argv[1], argv[2]);
     
-    while (1) {
-        int socket = accept_connection(sockFd, &clientFd);
-        if (socket < 0) {
-            perror("listen socket bind error\n");
-            return EXIT_FAILURE;
-        }
-        int pid = fork();
-        if (pid == 0) {
-            close(sockFd);
-            while (1) {
-                memset(msg, 0, sizeof(msg));
-                ssize_t byteCount = recv_message(clientFd, msg, BUFSIZE);
-                sentMsg = (message *) msg;
-    
-                dup2(clientFd, fileno(stdout));
-    
-                fprintf(stderr, "msg received: func %s ,args %s \n", sentMsg->function, sentMsg->arguments);
-    
-                strcpy(msgCopy, sentMsg->arguments);
-                int result = isCommandValid(sentMsg->function);
-                fprintf(stderr, "s:%s   d:%d \n", sentMsg->arguments, result);
-    
-                if (result == 1) {
-                    if (isInputLengthValid(sentMsg->arguments, 3)) {
-                        sprintf(answer, "%d", addInts(msgCopy));
-                    } else {
-                        sprintf(answer, "add needs 2 numbers");
-                    }
-                } else if (result == 2) {
-                    if (isInputLengthValid(sentMsg->arguments, 3)) {
-                        sprintf(answer, "%d", multiplyInts(msgCopy));
-                    } else {
-                        sprintf(answer, "multiple needs 2 numbers");
-                    }
-                } else if (result == 3) {
-                    if (isInputLengthValid(sentMsg->arguments, 3)) {
-                        strcpy(msg, msgCopy);
-                        if (isValidDivition(msgCopy)) {
-                            sprintf(answer, "%f", divideFloats(msg));
-                        } else {
-                            sprintf(answer, "Error: Division by zero");
-                        }
-                    } else {
-                        sprintf(answer, "divide needs 2 numbers");
-                    }
-                } else if (result == 4) {
-                    if (isInputLengthValid(sentMsg->arguments, 2)) {
-                        sleeps(msgCopy);
-                        sprintf(answer, " ");
-                    } else {
-                        sprintf(answer, "sleep needs 1 number");
-                    }
-                } else if (result == 5) {
-                    if (isInputLengthValid(sentMsg->arguments, 2)) {
-                        sprintf(answer, "%lu", factorial(msgCopy));
-                    } else {
-                        sprintf(answer, "factorial needs 1 number");
-                    }
-                } else if (result == 6) {
-                    sprintf(answer, " ");
-                    close(socket);
-                    return EXIT_SUCCESS;
-                } else if (result == 7) {
-                    sprintf(answer, " ");
-                    fprintf(stderr, "here %d %d\n", getpid(), getppid());
-                    kill(0, SIGTERM); //we want to kill all the current processes so we use 0
-                } else {
-                    sprintf(answer, "Error: Command \"%s\" not found", sentMsg->function);
+    int processChecker = fork(); //have a process to pool the shared memory in order to check if all programs are done when calling shutdown
+    if (processChecker == 0) {
+        int canShutdown = 0;
+        while (1) {
+            if (processes[0] == 0) {
+                close(server);
+                fprintf(stderr, "prearing shutdown, waiting on process to finish \n");
+                if (isEqual(processes)) {
+                    canShutdown = 1;
                 }
-                
-                if (byteCount <= 0) {
+                if (canShutdown) {
+                    kill(0, SIGTERM);
+                    free(processes);
                     break;
                 }
-                send_message(clientFd, answer, strlen(answer));
-                // indicate end of command output
-                fflush(stdout);
             }
-        } else {
-            //close the socket for the parent as we dont need it
-            close(socket);
+            
+            for (int i = 0; i < NUMBER_CLIENT; ++i) {
+                fprintf(stderr, "c%d, s%d | ", i, processes[i]);
+            }
+            printf("\n");
+            sleep(1);
         }
+    } else {
+        //this is where we accept client calls
+        while (1) {
+            int socket = accept_connection(sockFd, &clientFd);
+            if (socket < 0) {
+                perror("listen socket bind error\n");
+                return EXIT_FAILURE;
+            }
+            
+            int pid = fork();
+            if (pid == 0) {
+                close(sockFd);
+                while (1) {
+                    memset(msg, 0, sizeof(msg));
+                    
+                    ssize_t byteCount = recv_message(clientFd, msg, BUFSIZE);
+                    sentMsg = (message *) msg;
+                    dup2(clientFd, fileno(stdout));
+//                fprintf(stderr, "msg received: func %s ,args %s  counter %d\n", sentMsg->function, sentMsg->arguments, processCounter);
+                    processes[processCounter] = 1;
+                    fprintf(stderr, "counter %d \n", processCounter);
+                    strcpy(msgCopy, sentMsg->arguments);
+                    int result = isCommandValid(sentMsg->function);
+//                fprintf(stderr, "s:%s   d:%d \n", sentMsg->arguments, result);
+                    if (result == 1) {
+                        if (isInputLengthValid(sentMsg->arguments, 3)) {
+                            sprintf(answer, "%d", addInts(msgCopy));
+                        } else {
+                            sprintf(answer, "add needs 2 numbers");
+                        }
+                    } else if (result == 2) {
+                        if (isInputLengthValid(sentMsg->arguments, 3)) {
+                            sprintf(answer, "%d", multiplyInts(msgCopy));
+                        } else {
+                            sprintf(answer, "multiple needs 2 numbers");
+                        }
+                    } else if (result == 3) {
+                        if (isInputLengthValid(sentMsg->arguments, 3)) {
+                            strcpy(msg, msgCopy);
+                            if (isValidDivition(msgCopy)) {
+                                sprintf(answer, "%f", divideFloats(msg));
+                            } else {
+                                sprintf(answer, "Error: Division by zero");
+                            }
+                        } else {
+                            sprintf(answer, "divide needs 2 numbers");
+                        }
+                    } else if (result == 4) {
+                        if (isInputLengthValid(sentMsg->arguments, 2)) {
+                            sleeps(msgCopy);
+                            sprintf(answer, " ");
+                        } else {
+                            sprintf(answer, "sleep needs 1 number");
+                        }
+                    } else if (result == 5) {
+                        if (isInputLengthValid(sentMsg->arguments, 2)) {
+                            sprintf(answer, "%lu", factorial(msgCopy));
+                        } else {
+                            sprintf(answer, "factorial needs 1 number");
+                        }
+                    } else if (result == 6) {
+                        sprintf(answer, " ");
+                        processes[processCounter] = 0;  // indicates client is done
+                        return EXIT_SUCCESS;
+                    } else if (result == 7) {
+                        sprintf(answer, " ");
+                        processes[0] = 0; //send shutdown signal
+                        processes[processCounter] = 0; // indicates client is done
+                        return EXIT_SUCCESS;
+                    } else {
+                        sprintf(answer, "Error: Command \"%s\" not found", sentMsg->function);
+                    }
+                    
+                    if (byteCount <= 0) {
+                        break;
+                    }
+                    send_message(clientFd, answer, strlen(answer));
+                    // indicate end of command output
+                    fflush(stdout);
+                    processes[processCounter] = 0; // indicates client is done
+                }
+            } else {
+                //close the socket for the parent as we dont need it
+                close(socket);
+            }
+            processCounter++;
+        }
+        
+        
     }
     
 }
--- Assignment-1/readme.md
+++ Assignment-1/readme.md
@@ -8,7 +8,4 @@
 ## Notes
 The code has been tested and it works on `teaching.cs.mcgill.ca`
 
-* Assume that there are multiple clients connected to the server. If one of them write `shutdown`, it will terminate the server. 
-Then, if the other connected client  tries to input a command to the server, their session will terminate. Also, no new clients will be able to connect once the server is shutdown. 
-
-* Apparently we had to do struct passing between the client and server, I knew this a bit too late and I simply wrapped the message within a struct and sent it. Otherwise, I had to modify a lot of my code. 
+* message struct is declared in `a1_lib.h` in order to both the server and client to know about the format. 
