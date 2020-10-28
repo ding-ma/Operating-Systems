289b53097a131081f7da9ffab7c8398f9e9c3040
ding-ma <ding.ma@mail.mcgill.ca>
2020-10-28 13:08:30 -0400

    trying to generate .so file

--- Assignment-1/small_server.c
+++ Assignment-1/small_server.c
@@ -9,8 +9,7 @@
 int main(void) {
     int sockfd, clientfd;
     char msg[BUFSIZE];
-    const char *greeting = "hello, world";
-    int running = 1;
+    char greeting[BUFSIZE];
     
     if (create_server("0.0.0.0", 6969, &sockfd) < 0) {
         fprintf(stderr, "oh no\n");
@@ -24,16 +23,22 @@ int main(void) {
     srand(time(NULL)); //used to simulate read()
     printf("connection accepted \n");
     
+    int i = 0;
     while (strcmp(msg, "quit\n")) {
         memset(msg, 0, sizeof(msg));
-//        ssize_t byte_count = recv_message(clientfd, msg, BUFSIZE);
-//        if (byte_count > 0) {
-//            printf("Client: %s\n", msg);
-//        }
+        ssize_t byte_count = recv_message(clientfd, msg, BUFSIZE);
+        if (byte_count > 0) {
+            printf("Client: %s\n", msg);
+        }
+    
         int simulateRead = rand() % 10 + 1;
-        printf("reading for %d\n", simulateRead);
+        sprintf(greeting, "hello world from server %d", i);
+        printf("blocking read for %d\n", simulateRead);
         send_message(clientfd, greeting, strlen(greeting));
-        sleep(2);
+//        sleep(simulateRead);
+        i++;
+        memset(greeting, 0, sizeof(greeting));
+    
     }
     
     return 0;
---
+++ Assignment-2/test6.c
@@ -0,0 +1,45 @@
+#include "sut.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void hello2();
+
+void hello1() {
+    int i;
+    printf("Start\n");
+    sut_yield();
+    sut_open(HOST, PORT);
+    char sbuf[128];
+    char *str;
+    for (i = 0; i < 100; i++) {
+        sut_yield();
+        sprintf(sbuf, "Hello world!, message from SUT-One i = %d\n", i);
+        sut_write(sbuf, strlen(sbuf));
+        printf("Hello world!, this is SUT-One: %d\n", i);
+        str = sut_read();
+        printf("%s", str);
+    }
+    sut_close();
+    sut_create(hello2);
+    sut_exit();
+}
+
+void hello2() {
+    int i;
+    for (i = 0; i < 100; i++) {
+        printf("Hello world!, this is SUT-Two: %d\n", i);
+        sut_yield();
+        if (i == 50) {
+            sut_exit();
+            return;
+        }
+    }
+}
+
+int main() {
+    sut_init();
+    sut_create(hello2);
+    sut_create(hello1);
+    sut_shutdown();
+}
