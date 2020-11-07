2eb835ac6a21f73337b3d350c88a6786d4d14f8d
ding-ma <ding.ma@mail.mcgill.ca>
2020-11-07 16:01:25 -0500

    a3 starter code

---
+++ Assignment-3/a3_test.c
@@ -0,0 +1,130 @@
+/*
+ * =====================================================================================
+ *
+ *	Filename:  		a3_test.c
+ *
+ * 	Description:	Example of testing code of MyMalloc.
+ *
+ *  Version:  		2.0
+ *  Created:  		6/11/2020 9:30:00 AM	
+ *  Compiler:  		gcc
+ *
+ *  Authors:  		Devarun Bhattacharya, 
+ * 					Mohammad Mushfiqur Rahman
+ * 
+ * 	Intructions:	Please address all the "TODO"s in the code below and modify 
+ * 					them accordingly. No need to modify anything else, unless you 
+ * 					find a bug in the tester! Don't modify the tester to circumvent 
+ * 					the bug in your code!
+ * =====================================================================================
+ */
+
+/* Includes */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sma.h"
+
+int main(int argc, char *argv[]) {
+    int i, count = 0;
+    void *ptr, *limitafter = NULL, *limitbefore = NULL;
+    char *c[32], *ct;
+    
+    // Test 1: Find the holes
+    puts("Test 1: Hole finding test....");
+    
+    // Allocating 32 kbytes of memory..
+    for (i = 0; i < 32; i++)
+        c[i] = (char *) sma_malloc(1024);
+    // Now deallocating some of the slots ..to free
+    for (i = 10; i < 18; i++)
+        sma_free(c[i]);
+    
+    // Allocate some storage .. this should go into the freed storage
+    ct = (char *) sma_malloc(5 * 1024);
+    
+    // Testing if you are finding the available holes
+    if (ct < c[31])
+        puts("\t\t\t\t Passed\n");
+    else
+        puts("\t\t\t\t Failed\n");
+    
+    // Test 2: Program Break expansion Test
+    puts("Program break expansion test...");
+    
+    count = 0;
+    for (i = 1; i < 40; i++) {
+        limitbefore = sbrk(0);
+        ptr = sma_malloc(1024 * 32 * i);
+        limitafter = sbrk(0);
+        
+        if (limitafter > limitbefore)
+            count++;
+    }
+    
+    // Testing if the program breaks are incremented correctly
+    if (count > 0 && count < 40)
+        puts("\t\t\t\t Passed");
+    else
+        puts("\t\t\t\t Failed");
+    
+    // Test 3: Worst Fit Test
+    puts("Test 3: Check for Worst Fit algorithm.... ");
+    // Sets Policy to Worst Fit
+    sma_mallopt(WORST_FIT);
+    
+    // Allocating 512 kbytes of memory..
+    for (i = 0; i < 32; i++)
+        c[i] = (char *) sma_malloc(16 * 1024);
+    
+    // Now deallocating some of the slots ..to free
+    // One chunk of 5x16 kbytes
+    sma_free(c[31]);
+    sma_free(c[30]);
+    sma_free(c[29]);
+    sma_free(c[28]);
+    sma_free(c[27]);
+    
+    // One chunk of 2x16 kbytes
+    sma_free(c[20]);
+    sma_free(c[19]);
+    
+    // One chunk of 3x16 kbytes
+    sma_free(c[10]);
+    sma_free(c[9]);
+    sma_free(c[8]);
+    
+    // One chunk of 3x16 kbytes
+    sma_free(c[5]);
+    sma_free(c[4]);
+    
+    char *cp2 = sma_malloc(16 * 1024 * 2);
+    
+    // Testing if the correct hole has been allocated
+    if (cp2 == c[27] || cp2 == c[28] || cp2 == c[29] || cp2 == c[30])
+        puts("\t\t\t\t Passed");
+    else
+        puts("\t\t\t\t Failed");
+    
+    sma_free(cp2);
+    
+    // Test 4: Next Fit Test
+    puts("Test 4:Check for Next Fit algorithm.... ");
+    // Sets Policy to Next Fit
+    sma_mallopt(NEXT_FIT);
+    
+    char *cp3 = sma_malloc(16 * 1024 * 3);
+    char *cp4 = sma_malloc(16 * 1024 * 2);
+    
+    // Testing if the correct holes have been allocated
+    if (cp3 == c[8])
+        if (cp4 == c[19])
+            puts("\t\t\t\t Passed");
+        else
+            puts("\t\t\t\t Failed");
+    
+    puts("Print SMA Statistics:");
+    sma_mallinfo();
+    
+    return (0);
+}
